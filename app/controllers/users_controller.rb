84e75ae910d1a8a56cf89efe2adef2b11f1c8247
Enock <enock.mokua@student.moringaschool.com>
2023-03-20 01:13:42 +0300

    add private methdos to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,10 @@ class UsersController < ApplicationController
       render json: @employee.errors, status: :unprocessible_entity
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
