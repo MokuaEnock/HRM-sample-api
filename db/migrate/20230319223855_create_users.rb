4ddd2cfe272fc108712262ddb8cdfdfd2af3bf2a
Enock <enock.mokua@student.moringaschool.com>
2023-03-20 01:40:50 +0300

    add roles mask to users model

--- db/migrate/20230319223855_create_users.rb
+++ db/migrate/20230319223855_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :name
       t.string :email
       t.string :password_digest
+      t.integer :roles_mask, default: 0
 
       t.timestamps
     end
