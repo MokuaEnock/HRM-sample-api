ec6787e7e5c8d9f91f8be6cd904cf0e76b4d1815
Enock <enock.mokua@student.moringaschool.com>
2023-03-20 02:00:40 +0300

    set up asociation between role and user models

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,11 @@
 class Role < ApplicationRecord
+  has_and_belongs_to_many :users
+
+  def self.roles
+    %[admin employer employee]
+  end
+
+  def self.roles_mask
+    eolws.each_with_index.reduce({}) { |h, (r, i)| h[r.to_sym] = 1 << i; h }
+  end
 end
