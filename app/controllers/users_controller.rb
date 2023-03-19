50e2ca36fffd91e3c0fd4545f7f28b61181625b9
Enock <enock.mokua@student.moringaschool.com>
2023-03-20 01:12:22 +0300

    method create employee

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,12 @@
 class UsersController < ApplicationController
-
+  def create_employee
+  @employer = Employer.find(params[:employer_id])
+  @employee = @employer.employees.create(user_params)
+    if @employee.save
+      UserMailer.with(employer: employee).new_employee_email.deliver_now
+      render json: {employee: @employee, status: :created}
+    else
+      render json: @employee.errors, status: :unprocessible_entity
+    end
+  end
 end
