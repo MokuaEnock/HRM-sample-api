807b52c992be61e8c7449e1748714393f1811413
Enock <enock.mokua@student.moringaschool.com>
2023-03-20 01:17:07 +0300

    add new employee email method to user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+
+  def new_employee_email
+      @eployee = params[:employee]
+      mail(to: @employee.email, subject: 'Your new account details')
+  end
 end
